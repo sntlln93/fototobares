64ba48e51351ea5d6a3c282b67053d3aa52e353e
Matias Santillan <sntlln.93@gmail.com>
2025-02-21 12:46:13 -0300

    Fix bad ip address for load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,10 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: [
-            '192.168.1.1',
-            '10.0.0.0/8',
-        ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
