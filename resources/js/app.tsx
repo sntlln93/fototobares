21e15cbaa58f85a40d2e1f68e6f8e6155beae4e0
Matias Santillan <sntlln.93@gmail.com>
2025-03-02 18:53:26 -0300

    fix: dark mode doesnt load on start

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -23,3 +24,5 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+initializeTheme();
