234fdf3665f2e1ee9e149f6d88af0256e38a4d05
Matias Santillan <sntlln.93@gmail.com>
2025-03-02 21:20:30 -0300

    fix: csrf is refreshing to often

--- resources/js/hooks/use-csrf-auto-refresh.tsx
+++ resources/js/hooks/use-csrf-auto-refresh.tsx
@@ -1,14 +1,26 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
+const TOKEN_REFRESH_INTERVAL = 90 * 60 * 1000; // 90 minutes in milliseconds
+const STORAGE_KEY = 'lastCsrfRefreshTime';
+
 const useCsrfAutoRefresh = () => {
     useEffect(() => {
         const handleFocus = async () => {
-            try {
-                await axios.get('/sanctum/csrf-cookie'); // Refresh CSRF token
-            } catch (error) {
-                console.error('CSRF token expired. Reloading page...');
-                window.location.reload(); // Reload if CSRF token is invalid
+            const lastRefresh = localStorage.getItem(STORAGE_KEY);
+            const now = Date.now();
+
+            if (
+                !lastRefresh ||
+                now - parseInt(lastRefresh, 10) >= TOKEN_REFRESH_INTERVAL
+            ) {
+                try {
+                    await axios.get('/sanctum/csrf-cookie'); // Refresh CSRF token
+                    localStorage.setItem(STORAGE_KEY, now.toString()); // Update last refresh time
+                } catch (error) {
+                    console.error('CSRF token expired. Reloading page...');
+                    window.location.reload(); // Reload if CSRF token is invalid
+                }
             }
         };
 
