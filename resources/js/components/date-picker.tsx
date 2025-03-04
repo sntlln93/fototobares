c14a39a29756afdbc9ebb3367cb5e67921bbc2af
Matias Santillan <sntlln.93@gmail.com>
2025-03-04 00:13:34 -0300

    feat: add date-picker

---
+++ resources/js/components/date-picker.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-react';
+
+type DatePickerProps = {
+    placeholder?: string;
+    date: Date;
+    setDate: (date: Date | undefined) => void;
+};
+
+export function DatePicker({ placeholder, date, setDate }: DatePickerProps) {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant={'outline'}
+                    className={cn(
+                        'mt-1 w-full justify-start text-left font-normal',
+                        !date && 'text-muted-foreground',
+                    )}
+                >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date
+                        ? format(date, 'PPP')
+                        : placeholder && <span>{placeholder}</span>}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    );
+}
