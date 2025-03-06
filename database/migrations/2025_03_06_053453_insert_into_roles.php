396923c72ab4668005485a87540aca95d01e2fe8
Matias Santillan <sntlln.93@gmail.com>
2025-03-06 12:13:15 +0000

    fix: add missing timestamp on system data insert

---
+++ database/migrations/2025_03_06_053453_insert_into_roles.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $created_at = new DateTime;
+
+        $roles = [
+            ['name' => 'master', 'created_at' => $created_at],
+            ['name' => 'administración', 'created_at' => $created_at],
+            ['name' => 'oficina', 'created_at' => $created_at],
+            ['name' => 'edición', 'created_at' => $created_at],
+            ['name' => 'taller', 'created_at' => $created_at],
+        ];
+
+        foreach ($roles as $role) {
+            DB::insert('INSERT INTO roles(name, created_at) VALUES (:name, :created_at)', $role);
+        }
+    }
+};
