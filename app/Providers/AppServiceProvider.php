658d1cd0cea5a63a2043f7c6467d1c8e3ba3307e
Matias Santillan <sntlln.93@gmail.com>
2025-03-09 05:01:17 +0000

    internal: add phpstan, configure pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
@@ -25,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //$this->configureCommands();
-
         $this->configureModels();
 
         $this->configureURLs();
@@ -38,30 +37,25 @@ class AppServiceProvider extends ServiceProvider
         $this->configureVite();
     }
 
-    private function configureVite()
+    private function configureVite(): void
     {
         Vite::prefetch(concurrency: 3);
     }
 
-    private function configureURLs()
+    private function configureURLs(): void
     {
         if ($this->isProduction()) {
             URL::forceScheme('https');
         }
     }
 
-    private function configureCommands()
-    {
-        DB::prohibitDestructiveCommands($this->isProduction());
-    }
-
-    private function configureModels()
+    private function configureModels(): void
     {
         Model::shouldBeStrict();
         Model::unguard();
     }
 
-    private function isProduction()
+    private function isProduction(): bool
     {
         return $this->app->environment() === 'production';
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,31 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-use App\Models\Combo;
-use App\Models\Product;
-use App\Models\School;
-use App\Models\Stockable;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        User::factory(10)->create();
-
-        Product::factory(10)->create();
-        Combo::factory(3)->create();
-
-        Stockable::factory(20)->create();
-
-        DB::insert('insert into product_stockable(product_id, stockable_id) values (1,1)');
-
-        School::factory(3)->withClassRooms()->withPrincipal()->withAddress()->create();
-    }
+    public function run(): void {}
 }
