5a3db34413accfdc271c9ef3995d1d38b6a5c1a5
Matias Santillan <sntlln.93@gmail.com>
2026-07-08 21:53:16 -0300

    feat(seeders): seed a full demo dataset for manual testing (#83)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SchoolSeeder::class,
+            DemoSeeder::class,
         ]);
     }
 }
