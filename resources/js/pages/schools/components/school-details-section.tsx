9d62f125feaebe3f86e945367270fcdc3118c6b5
Matias Santillan <sntlln.93@gmail.com>
2026-07-13 19:24:12 -0300

    fix(schools): align school level accent between select, type and data

--- resources/js/pages/schools/components/school-details-section.tsx
+++ resources/js/pages/schools/components/school-details-section.tsx
@@ -74,7 +74,7 @@ export function SchoolDetailsSection({
                             <SelectValue />
                         </SelectTrigger>
                         <SelectContent>
-                            <SelectItem value="Jardin">Jardín</SelectItem>
+                            <SelectItem value="Jardín">Jardín</SelectItem>
                             <SelectItem value="Primaria">Primaria</SelectItem>
                             <SelectItem value="Secundaria">
                                 Secundaria
